a3f447a463d282bc1d98c0b0f50222beda85c976
Pascal Kollie <blockroyaltyemail@gmail.com>
2018-04-24 19:08:51 -0400

    Lambda for get git repo file and uploding it to s3

--- upload-repo-lambda.py
+++ upload-repo-lambda.py
@@ -1,19 +1,32 @@
- import boto3
- from botocore.client import Config
- import StringIO
- import zipfile
- import mimetypes
- s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+import boto3
+import StringIO
+import zipfile
+import mimetypes
 
-website_bucket = s3.Bucket('website.blockroyalty.com')
-build_bucket = s3.Bucket('websitebuild.blockroyalty.com')
+def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:240748614592:DeployBlockWebsiteTopic')
 
-blockroyalty_zip = StringIO.StringIO()
-build_bucket.download_fileobj('blockroyaltybuild.zip', blockroyalty_zip)
+    try:
+        s3 = boto3.resource('s3')
 
-with zipfile.ZipFile(blockroyalty_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        website_bucket.upload_fileobj(obj, nm, 
-        ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        website_bucket.Object(nm).Acl().put(ACL='public-read')
+        website_bucket = s3.Bucket('website.blockroyalty.com')
+        build_bucket = s3.Bucket('websitebuild.blockroyalty.com')
+
+        website_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('blockroyaltybuild.zip', website_zip)
+
+        with zipfile.ZipFile(website_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                website_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                website_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print "Job done!"
+        topic.publish(Subject="Website Deployed", Message="Website deployed successfully!")
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="The Website was not updated successfully")
+        raise
+
+    return 'Hello from Lambda' 
