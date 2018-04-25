449ebb2e55118f9271ff3cacd14dd0e299291c0e
Pascal Kollie <blockroyaltyemail@gmail.com>
2018-04-24 20:02:21 -0400

    add codepipeline

--- upload-repo-lambda.py
+++ upload-repo-lambda.py
@@ -6,15 +6,27 @@ import mimetypes
 def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:240748614592:DeployBlockWebsiteTopic')
-
+    location = {
+        "bucketName":'websitebuild.blockroyalty.com',
+        "objectKey": 'blockroyaltybuild.zip'
+    }
     try:
+        job = event.get("CodePipeline.job")
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if  artifact["name"] == "MyAppBuild":
+                     location = artifact["Location"]["s3Location"]
+
+        print "Building website from " + str(location)
+
         s3 = boto3.resource('s3')
 
         website_bucket = s3.Bucket('website.blockroyalty.com')
-        build_bucket = s3.Bucket('websitebuild.blockroyalty.com')
+        build_bucket = s3.Bucket(location["bucketName"])
 
         website_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('blockroyaltybuild.zip', website_zip)
+        build_bucket.download_fileobj(location["objectKey"], website_zip)
 
         with zipfile.ZipFile(website_zip) as myzip:
             for nm in myzip.namelist():
@@ -25,6 +37,9 @@ def lambda_handler(event, context):
 
         print "Job done!"
         topic.publish(Subject="Website Deployed", Message="Website deployed successfully!")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobID=jobID["id"])
     except:
         topic.publish(Subject="Portfolio Deploy Failed", Message="The Website was not updated successfully")
         raise
